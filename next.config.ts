5b1c1d7701cc89cfdfc145cbef385c66b4264d9b
dflora <yaniksazcetin@gmail.com>
2026-04-05 20:04:04 +0300

    fix: revert static export format to restore Hostinger automated github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/tr',
+        permanent: false,
+      },
+      {
+        // Redirect any path that DOES NOT start with our locales or next.js internals
+        source: '/:path((?!tr|en|es|_next|api|.*\\.).*)',
+        destination: '/tr/:path',
+        permanent: false,
+      }
+    ];
   },
 };
 
